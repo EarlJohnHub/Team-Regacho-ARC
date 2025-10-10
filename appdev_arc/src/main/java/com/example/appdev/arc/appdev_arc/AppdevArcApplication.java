97167e71e91948a7acf76c691f883c4e54ba3cf7
John Earl C. Echavez <johnearl.echavez@cit.edu>
2025-10-10 17:25:32 +0800

    Initialization of the project

---
+++ appdev_arc/src/main/java/com/example/appdev/arc/appdev_arc/AppdevArcApplication.java
@@ -0,0 +1,13 @@
+package com.example.appdev.arc.appdev_arc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppdevArcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppdevArcApplication.class, args);
+	}
+
+}
