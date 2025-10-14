867e22afffe6a063866e07e85ad983697b82af03
harley <harley.reyes@cit.edu>
2025-10-14 12:59:53 +0800

    modified to have RequiredArgsConstructor annotation

--- appdev_arc/src/main/java/com/example/appdev/arc/appdev_arc/controller/StudentController.java
+++ appdev_arc/src/main/java/com/example/appdev/arc/appdev_arc/controller/StudentController.java
@@ -2,6 +2,7 @@ package com.example.appdev.arc.appdev_arc.controller;
 
 import java.util.List;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,10 +19,10 @@ import com.example.appdev.arc.appdev_arc.service.StudentService;
 
 @RestController
 @RequestMapping("/students")
+@RequiredArgsConstructor
 public class StudentController {
 
-    @Autowired
-    private StudentService studentService;
+    private final StudentService studentService;
 
     @GetMapping
     public List<StudentEntity> getAllStudents() {
