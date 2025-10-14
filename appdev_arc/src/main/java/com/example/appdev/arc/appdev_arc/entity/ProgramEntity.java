ea71a4a4de0da599fbe25f8fb8733c929f864784
harley <harley.reyes@cit.edu>
2025-10-14 12:55:37 +0800

    added one to many relationship to ProgramCourseEntity and many to one relationship to DepartmentEntity

---
+++ appdev_arc/src/main/java/com/example/appdev/arc/appdev_arc/entity/ProgramEntity.java
@@ -0,0 +1,32 @@
+package com.example.appdev.arc.appdev_arc.entity;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "program")
+@Data
+public class ProgramEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int programID;
+
+    @Column(name = "programName")
+    private String programName;
+
+    @ManyToOne
+    @JoinColumn(name = "department_id", referencedColumnName = "departmentID")
+    @JsonBackReference
+    private DepartmentEntity department;
+
+    @OneToMany(mappedBy = "program", cascade = CascadeType.ALL, orphanRemoval = true)
+    @JsonManagedReference("program-programCourses")
+    private List<ProgramCoursesEntity> programCoursesEntities = new ArrayList<>();
+
+}
