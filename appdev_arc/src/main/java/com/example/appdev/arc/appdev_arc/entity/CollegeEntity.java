e2d641f9970f9a55458ce0b904330acc063324b1
harley <harley.reyes@cit.edu>
2025-10-14 12:49:51 +0800

    added one to many relationship to DepartmentEntity

---
+++ appdev_arc/src/main/java/com/example/appdev/arc/appdev_arc/entity/CollegeEntity.java
@@ -0,0 +1,28 @@
+package com.example.appdev.arc.appdev_arc.entity;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@Entity
+@Table(name = "college")
+public class CollegeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int collegeID;
+
+    @Column(name = "college_name")
+    private String collegeName;
+
+    @Column(name = "college_code")
+    private int collegeCode;
+
+    @OneToMany(mappedBy = "college", cascade = CascadeType.ALL, orphanRemoval = true)
+    @JsonManagedReference
+    private List<DepartmentEntity> departmentEntities = new ArrayList<>();
+
+}
